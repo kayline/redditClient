3361d8ac8c5252d6957247b63702330081bc5136
Charlie Bevis and Shatarupa Nandi <pair+cbevis+snandi@pivotallabs.com>
2014-03-11 09:58:04 -0700

    Initial Commit

---
+++ redditClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  redditClient
+//
+//  Created by pivotal on 3/11/14.
+//  Copyright (c) 2014 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
